1f02ba6709a83ac269afc22ff023c893534fa4a3
Geert-Johan Riemer <geertjohan@geertjohan.net>
2018-01-15 15:24:29 +0100

    Add terrible.rb formula

---
+++ terrible.rb
@@ -0,0 +1,11 @@
+class TerribleJob < Formula
+  homepage "http://server-001.terrible2.production.operations.aws.sanomahost.nl"
+  url "http://server-001.terrible2.production.operations.aws.sanomahost.nl/download/terrible-darwin.tar.gz"
+  version "0.0.13"
+  sha256 "fdsafdsa"
+
+  def install
+      bin.install "terrible"
+      ohai "Have a terrible day"
+  end
+end

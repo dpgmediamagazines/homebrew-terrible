a4693b8405c57b83bc25f1004fad855a3d8d3bf1
Geert-Johan Riemer <geertjohan@geertjohan.net>
2018-01-23 13:34:38 +0100

    Bump to version v2.0.0

--- terrible.rb
+++ terrible.rb
@@ -1,8 +1,8 @@
 class Terrible < Formula
   homepage "http://server-001.terrible2.production.operations.aws.sanomahost.nl"
   url "http://server-001.terrible2.production.operations.aws.sanomahost.nl/download/terrible-darwin.tar.gz"
-  version "0.0.15"
-  sha256 "c212936a84fc226dded3a0ddfdff3d059a998128ed0810c6a51e00b2e0497442"
+  version "2.0.0"
+  sha256 "c0567f74c9c0c7917023f26e017a0f4014e4b74da9835813cb2d8bd798b3f2d1"
 
   def install
       bin.install "terrible"

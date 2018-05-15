5a7c5b7e101cb1fa823f86a21c7a06aed5c3d90d
GitLab Runner <gitlab-runner@runner-001.gitlab_runner.production.operations.aws.sanomahost.nl>
2018-05-15 14:45:23 +0200

    Bump to version v2.0.19

--- terrible.rb
+++ terrible.rb
@@ -1,8 +1,8 @@
 class Terrible < Formula
   homepage "http://server-001.terrible2.production.operations.aws.sanomahost.nl"
   url "http://server-001.terrible2.production.operations.aws.sanomahost.nl/download/terrible-darwin.tar.gz"
-  version "2.0.18"
-  sha256 "f1fe758c9d257958b5287e61928afb5be96b44815c57795e994691048c076967"
+  version "2.0.19"
+  sha256 "5a5fd9f7cd02ddd0dafd7f2ba5166f71e2f5ff4bfd25e834aa2200a83403a091"
 
   def install
       bin.install "terrible"

8acb344a8c8ea7e68b51882f07e233ece7ad101f
Bamboo <bamboo@bamboo>
2020-10-15 14:28:45 +0000

    Bump to version v2.51.0

--- terrible.rb
+++ terrible.rb
@@ -1,8 +1,8 @@
 class Terrible < Formula
   homepage "https://terrible.sanoma.tech/"
   url "https://terrible.sanoma.tech/download/terrible-darwin.tar.gz"
-  version "2.50.0"
-  sha256 "15dc8c42c0d1d779481f9589af04ba2479014fbf8076adf388f258f3675b5aa8"
+  version "2.51.0"
+  sha256 "857efe5d9a2a5277d85b541471442467719edff3ed1e55d928c20eaedb81297b"
 
   def install
       bin.install "terrible"

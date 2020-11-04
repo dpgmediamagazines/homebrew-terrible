eef724aec34322884ca85d77b8eeffe14908db07
SVliet <35456780+SVliet@users.noreply.github.com>
2020-11-04 16:15:09 +0100

    updating link

--- terrible.rb
+++ terrible.rb
@@ -1,5 +1,5 @@
 class Terrible < Formula
-  homepage "https://terrible.sanoma.tech/"
+  homepage "https://terrible.dpgmm.tech/"
   url "https://terrible.dpgmm.tech/download/terrible-darwin.tar.gz"
   version "2.52.0"
   sha256 "baecced820d13e11b0cbbdc26164134b21dea56b2fc3c2a80773d04fcb10d822"

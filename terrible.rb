6e7a24d963e663ec86f77fee835cd2b9048c08ce
SVliet <35456780+SVliet@users.noreply.github.com>
2021-01-15 13:52:54 +0100

    Update terrible.rb

--- terrible.rb
+++ terrible.rb
@@ -1,7 +1,7 @@
 class Terrible < Formula
   homepage "https://terrible.dpgmm.tech/"
   url "https://terrible.dpgmm.tech/download/terrible-darwin.tar.gz"
-  version "b35e8e50ff805731f693728adc8a0e23c3609a74"
+  version "v2.56.0"
   sha256 "9f8de9c3683bd780d16870bb9deed8b415eee6d82af00f6411887d0bee95d7ed"
 
   def install

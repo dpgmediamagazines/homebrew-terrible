eda93f06674a215f98d31fa7efd0e654e44b4638
Sebastiaan van der Vliet <sebastiaan.vandervliet@sanoma.com>
2021-03-15 11:37:34 +0100

    up

--- terrible.rb
+++ terrible.rb
@@ -1,7 +1,7 @@
 class Terrible < Formula
   homepage "https://terrible.dpgmm.tech/"
   url "https://terrible.dpgmm.tech/download/terrible-darwin.tar.gz"
-  version "6c7f8f894ee3bb2958178062f4a00fcc25b70b6e"
+  version "v.2.57.0"
   sha256 "d727227d94a3fe0d74a61961019bfb2e1d63e61506862aeeb6fac4899dd105bd"
 
   def install

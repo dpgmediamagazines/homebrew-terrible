f1c1f3963c6179a49e17ddee3cbe5b7368b8ae18
Bamboo <bamboo@bamboo>
2021-06-16 08:48:28 +0000

    Bump to version 2.65.0

--- terrible.rb
+++ terrible.rb
@@ -1,9 +1,9 @@
 class Terrible < Formula
   homepage "https://terrible.dpgmm.tech/"
   url "https://terrible.dpgmm.tech/download/terrible-darwin.tar.gz"
-  version "2.64.0"
-  sha256 "692ef8e4027948be1b3996470fb1fd12d741ddb7046dd013348ebb927b2653ad"
-  #sha256 "692ef8e4027948be1b3996470fb1fd12d741ddb7046dd013348ebb927b2653ad"
+  version "2.65.0"
+  sha256 "4c25456b41aaaf585cdea9819d39859efc447fd5f42cfc8fd2960c83c04d22ef"
+  #sha256 "4c25456b41aaaf585cdea9819d39859efc447fd5f42cfc8fd2960c83c04d22ef"
 
   def install
       bin.install "terrible"
